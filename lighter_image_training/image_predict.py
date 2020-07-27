eef664c65b1489e2197bf12fe3177bcccf8941f8
hyoj0420 <hyoj0420@gmail.com>
2020-07-27 22:59:12 +0900

    predict whether lighter or not

---
+++ lighter_image_training/image_predict.py
@@ -0,0 +1,33 @@
+import os, re, glob
+import cv2
+import numpy as np
+import shutil
+from numpy import argmax
+from keras.models import load_model
+ 
+categories = ["lighter"]
+ 
+def Dataization(img_path):
+    image_w = 28
+    image_h = 28
+    img = cv2.imread(img_path)
+    img = cv2.resize(img, None, fx=image_w/img.shape[1], fy=image_h/img.shape[0])
+    return (img/256)
+ 
+src = []
+name = []
+test = []
+image_dir = "C:\\Users\\hyoj\\OneDrive\\Desktop\\lighter\\main\\lighter_image_training\\lighter_predict_smaple\\"
+for file in os.listdir(image_dir):
+    if (file.find('.png') is not -1):       
+        src.append(image_dir + file)
+        name.append(file)
+        test.append(Dataization(image_dir + file))
+ 
+ 
+test = np.array(test)
+model = load_model('Lighter.h5')
+predict = model.predict_classes(test)
+ 
+for i in range(len(test)):
+    print(name[i] + " : , Predict : "+ str(categories[predict[i]]))
