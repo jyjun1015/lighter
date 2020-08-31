796f2f48ed2fef72715b5eb7d9fac3eee8f367c0
hyoj0420 <hyoj0420@gmail.com>
2020-08-31 21:28:27 +0900

    add gdrive upload

--- lighter_image_training/getLighterCap_multi.py
+++ lighter_image_training/getLighterCap_multi.py
@@ -5,28 +5,67 @@ import io
 import os
 import time
 import multiprocessing
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+
+class GDrive :
+    def __init__(self):
+        try :
+            import argparse
+            flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+        except ImportError:
+            flags = None
+
+        # 현재는 py파일과 client_secret_drive.json 파일이 모두 사진폴더에 있어야 함.
+        # 이후 간단히 '.jpg' 확장자만 업로드할 수 있도록 바꿔주면 된다. 
+        os.chdir('C:\\Users\\cydph\\Desktop\\testUpload')       # 현재 디렉토리를 사진폴더로 변경
+        SCOPES = 'https://www.googleapis.com/auth/drive.file'   # [변경X] Google API 사용 로그인 링크
+        store = file.Storage('storage.json')                    # 현재 로그인된 드라이브 정보 로드.
+        creds = store.get()                                     # storage.json 파일 가져온다.
+
+        if not creds or creds.invalid:              # 만일 storage.json 파일이 없다면 생성해야 함.
+            print("make new storage data file ")    # 구글 로그인을 시도하고 storage.json 파일 생성.
+            flow = client.flow_from_clientsecrets('client_secret_drive.json', SCOPES)
+            creds = tools.run_flow(flow, store, flags) \
+                    if flags else tools.run(flow, store)
+
+        self.DRIVE = build('drive', 'v3', http=creds.authorize(Http()))
+        self.FOLDER = '1k0y5cctckms5m8F9UDg3mqrrkgIbg2Ax'        # 저장할 드라이브 폴더 고유 ID
+    
+    # 이전엔 폴더의 기존 이미지를 전부 업로드 했다면 이제는 파일을 하나로 특정하여 업로드
+    def upload(self, filename) :
+        metadata = {'name': filename,
+                    'parents':[self.FOLDER],                 # 사진 저장할 드라이브 내 폴더
+                    'mimeType': None                    # 데이터 타입은 아랫줄에서 결정.
+                    }
+
+        res = DRIVE.files().create(body=metadata, media_body=filename).execute()
 
 def getCapture(cap) :
     with picamera.PiCamera() as camera :
         camera.resolution = (416, 416)
         stream = io.BytesIO()
-
         
         for frame in camera.capture_continuous(stream, format="jpeg") :
             data = np.fromstring(stream.getvalue(), dtype=np.uint8)
             img = cv2.imdecode(data, 1)
-            cv2.imwrite("images/"+str(cap)+".png", img)
+            cv2.imwrite("images/"+str(cap)+".jpg", img)
             cap += 1
 
 def yolo(cap) :
-    net = cv2.dnn.readNet("custom-train-yolo_2000.weights", "custom-train-yolo.cfg")
+    gDrive = GDrive()
+
+    net = cv2.dnn.readNet("yolov3-tiny_3000.weights", "yolo3-tiny.cfg")
     classes = ["lighter"]
     layer_names = net.getLayerNames()
     output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+    prev = time.time()
     
     while True :
-        if os.path.isfile("images/"+str(cap)+".png") :
-            img = cv2.imread("images/"+str(cap)+".png")
+        if os.path.isfile("images/"+str(cap)+".jpg") :
+            img = cv2.imread("images/"+str(cap)+".jpg")
             try :
                 blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
                 net.setInput(blob)
@@ -52,15 +91,24 @@ def yolo(cap) :
                             confidences.append(float(confidence))
 
                 indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.3, 0.2)
-                if len(indexes) < 5 :
-                    os.remove("images/"+str(cap)+".png")
-                print("yes")
+
+                # 인식된 라이터가 다섯개 이상이면 업로드
+                if len(indexes) >= 5 :
+                    gDrive.upload(str(cap)+".jpg")
+
+                # 처리가 끝난 이미지는 무조건 삭제
+                os.remove("images/"+str(cap)+".jpg")
                 cap += 1
+                prev = time.time()
+
             except Exception as e :
                 print(str(e))
             
         else :
-            pass
+            if time.time() - prev > 10 :
+                return
+            else :
+                pass
         
 if __name__ == '__main__' :
     cap = 0
--- lighter_image_training/templateTest/templateTest.py
+++ lighter_image_training/templateTest/templateTest.py
@@ -6,30 +6,57 @@ def check(coord_ori, coord_result, target) :
     else :
         return False
 
-origin = cv2.imread("ligher_ori.jpg")
-hurt = cv2.imread("ligher_ori.jpg")
+def resize(file) :
+    target = cv2.imread(file)
+    target = cv2.resize(target, dsize=(0, 0), fx=0.5, fy=0.5, interpolation=cv2.INTER_LINEAR)
+    return target
 
-coords = [316, 361, 213, 340]
+origin = resize("origin3.jpg")
+dst = origin.copy()
+hurt = resize("fault4.jpg")
+dst2 = hurt.copy()
 
-hurt_modi = hurt.copy()
-hurt_modi = hurt[coords[2]:coords[3], coords[0]:coords[1]]
+#coords_origin = [[363, 452, 352, 594], [465, 544, 352, 596], 
+#        [559, 640, 357, 596], [658, 726, 358, 595], [750, 819, 358, 594]]
 
-result = cv2.matchTemplate(origin, hurt_modi, cv2.TM_SQDIFF_NORMED)
+#coords_origin = [[658, 735, 404, 657]]
 
-minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(result)
-x, y = minLoc
-h, w = hurt_modi.shape[0:2]
+coords_origin = [[226, 290, 311, 560], [311, 373, 314, 568], 
+        [403, 462, 316, 567], [485, 547, 318, 572], [573, 637, 320, 571],
+        [656, 718, 319, 571], [742, 802, 320, 574], [825, 889, 326, 578],
+        [906, 975, 324, 576], [994, 1056, 327, 578]]
 
-coords_result = [x, x+w, y, y+h]
-print("original coordinates : ", end='')
-print(coords)
+for i in range(10) :
+    coords = []
+    print("number", i)
 
-for i in range(4) :
-    print("predicted coordinates :", coords_result[i], end=' ')
-    if not check(coords[i], coords_result[i], 0.03) :
-        print("This is faulty!")
+    for k in range(4) :
+        coords.append(int(coords_origin[i][k]*0.5))
 
-dst = cv2.rectangle(origin, (x, y), (x +  w, y + h) , (0, 0, 255), 1)
-cv2.imshow("dst", origin)
+    hurt_modi = hurt.copy()
+    hurt_modi = hurt[coords[2]:coords[3], coords[0]:coords[1]]
+
+    result = cv2.matchTemplate(dst, hurt_modi, cv2.TM_SQDIFF_NORMED)
+
+    minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(result)
+    x, y = minLoc
+    h, w = hurt_modi.shape[0:2]
+
+    coords_result = [x, x+w, y, y+h]
+    print("original coordinates : ", end='')
+    print(coords)
+
+    for k in range(4) :
+        print("predicted coordinates :", coords_result[k], end=' ')
+        if not check(coords[k], coords_result[k], 0.03) :
+            print("This is faulty!")
+
+    cv2.putText(dst, str(i), (coords[0], coords[2]+10), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 1, (0, 255, 0))
+    cv2.putText(dst2, str(i), (x, y+10), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 1, (0, 255, 0))
+    dst = cv2.rectangle(dst, (coords[0], coords[2]), (coords[1], coords[3]) , (0, 0, 255), 1)
+    dst2 = cv2.rectangle(dst2, (x, y), (x +  w, y + h) , (0, 0, 255), 1)
+
+addh = cv2.hconcat([dst2, dst])
+cv2.imshow("dst", addh)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
